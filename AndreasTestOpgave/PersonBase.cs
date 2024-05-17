2bbfb4012583bb6922a256736a2fc8c18ebea657
Andreas Carl <andreascarl@hotmail.dk>
2024-05-17 12:12:30 +0200

    Add project files.

---
+++ AndreasTestOpgave/PersonBase.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp
+{
+    public struct Person
+    {
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Job { get; set; }
+        public string Name { get; set; }
+        public int Nummer { get; set; }
+    }
+}
---
+++ AndreasTestOpgave/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
+using System.Xml.Linq;
+using static MyApp.Program;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            List<Person> persons = new List<Person>
+            {
+                new Person(){Name = "Mathias", Age = 18, Address = "Nederen", Job = "Nederen", Birthday = new DateTime(2003,03,03) },
+
+            };
+
+            persons.Add(CreateNewPerson());
+
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Address);
+            Console.WriteLine(person.Job);
+            Console.WriteLine(person.Nummer);
+            Console.WriteLine(person.Birthday);
+        }
+
+        public static Person CreateNewPerson()
+        {
+            Console.Write("Write your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Write your age: ");
+            int age = Int32.Parse(Console.ReadLine());
+            Console.Write("Write your address: ");
+            string address = Console.ReadLine();
+            Console.Write("Write your job: ");
+            string job = Console.ReadLine();
+            Console.Write("Write your number: ");
+            int number = Int32.Parse(Console.ReadLine());
+            Console.Write("Write your birthday: ");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+
+            Person person = new Person();
+            person.Name = name;
+            person.Age = age;
+            person.Address = address;
+            person.Job = job;
+            person.Nummer = number;
+            person.Birthday = birthday;
+
+            Console.Write("Velkommen " + name + " til ACB's program");
+            Console.WriteLine("\n Din alder: " + +age +
+                "\n Din addresse: " + address +
+                "\n Dit job: " + job +
+                "\n Dit nummer: " + number +
+                "\n Din fødselsdag: " + birthday);
+
+            return person;
+        }
+        //  private static void Hello()
+        //{
+        // Console.WriteLine("Hej!");
+        //}
+    }
+
+
+
+
+
+}
+
+
+
