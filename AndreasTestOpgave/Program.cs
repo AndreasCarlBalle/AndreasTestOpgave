05fafbc70fb3102ea5413471958e12f9a9d3fafd
Andreas Carl <andreascarl@hotmail.dk>
2024-05-22 14:15:43 +0200

    nahuh

--- AndreasTestOpgave/Program.cs
+++ AndreasTestOpgave/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,28 +14,58 @@ namespace MyApp
 {
     public class Program
     {
+        static List<Person> _persons = null;
+
         static void Main(string[] args)
         {
-            Person person = new Person();
-
-            List<Person> persons = new List<Person>
+            // vi simuler/leger, at vi har hente 3 personer ind fra vores database.
+            _persons = new List<Person>
             {
-                new Person(){Name = "Mathias", Age = 18, Address = "Nederen", Job = "Nederen", Birthday = new DateTime(2003,03,03) },
+                new Person(){Name = "", Age = 18, Address = "Nederen", Job = "Nederen", Birthday = new DateTime(2003,03,03) },
                 new Person(){Name = "Andreas", Age = 20, Address = "Frederiksberg", Job = "TEC", Birthday = new DateTime(2004,03,03) },
                 new Person(){Name = "Harald Blåtand", Age = 1089, Address = "Jelling", Job = "Konge af Danmark", Birthday = new DateTime(932,04,02) }
             };
 
+            while (true)
+            {
+                Menu();
+                MenuSelection();
+            }
+        }
 
+        private static void MenuSelection()
+        {
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    Console.WriteLine("Create Person: Enter Values");
+                    _persons.Add(CreateNewPerson());
+                    break;
+                case 2:
+                    Console.WriteLine("View Person: Enter Values");
 
-            persons.Add(CreateNewPerson());
+                   break;
+                    case 3:
+                    Console.WriteLine("Update Person: Enter Values");
+                    break;
+                case 4:
+                    Console.WriteLine("Delete Person: Enter Values");  
+                    break;
+            }
+        }
 
+        private static void Menu()
+        {
+            Console.WriteLine("Enter 0 to Exit");
+            Console.WriteLine("Enter 1 to Create a person");
+            Console.WriteLine("Enter 2 to View all persons");
+            Console.WriteLine("Enter 3 to Update a person");
+            Console.WriteLine("Enter 4 to Delete a person");
+ 
 
-            Console.WriteLine(person.Name);
-            Console.WriteLine(person.Age);
-            Console.WriteLine(person.Address);
-            Console.WriteLine(person.Job);
-            Console.WriteLine(person.Nummer);
-            Console.WriteLine(person.Birthday);
         }
 
         public static Person CreateNewPerson()
@@ -68,17 +100,16 @@ namespace MyApp
 
             return person;
         }
-        //  private static void Hello()
-        //{
-        // Console.WriteLine("Hej!");
-        //}
+
     }
 
-
-
-
-
 }
+
+
+
+
+
+
 
 
 
